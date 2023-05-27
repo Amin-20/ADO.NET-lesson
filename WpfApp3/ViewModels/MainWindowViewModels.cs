1b21fe062ab7d798ef6ecbfcbbe78ff7a63bc4aa
Ataki_pw11@itstep.edu.az <Ataki_pw11@itstep.edu.az>
2023-05-28 00:17:39 +0400

    Add project files.

---
+++ WpfApp3/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfApp3.Commands;
+
+namespace WpfApp3.ViewModels
+{
+    public class MainWindowViewModels : BaseViewModel
+    {
+        RelayCommand InsertCommand { get; set; }
+        RelayCommand DeleteRelayCommand { get; set; }
+        RelayCommand UpdateRelayCommand { get; set; }
+        RelayCommand ShowAllRelayCommand { get; set; }
+
+        private TextBox id;
+
+        public TextBox Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private TextBox firstName;
+
+        public TextBox FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged(); }
+        }
+
+        private TextBox lastName;
+
+        public TextBox LastName
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged(); }
+        }
+
+        private ListBox authorsList;
+
+        public ListBox AuthorsList
+        {
+            get { return authorsList; }
+            set { authorsList = value; OnPropertyChanged(); }
+        }
+
+
+        public MainWindowViewModels()
+        {
+            AuthorsList= new ListBox();
+
+            InsertCommand = new RelayCommand(obj =>
+            {
+                AuthorsList.Items.Add(firstName.Text);
+
+
+            });
+
+
+            DeleteRelayCommand = new RelayCommand(obj =>
+            {
+
+
+
+            });
+
+
+            UpdateRelayCommand = new RelayCommand(obj =>
+            {
+
+
+            });
+
+            ShowAllRelayCommand = new RelayCommand(obj =>
+            {
+
+
+
+            });
+
+
+
+        
+        }
+    }
+}
