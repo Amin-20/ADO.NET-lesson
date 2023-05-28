b5aeaf136638e9d3f5e78f8c3ff346234e2e9a36
Ataki_pw11 <Ataki_pw11@STHQ0124-04>
2023-05-28 12:54:36 +0400

    View done

---
+++ WpfApp3/Repository/Repo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Repository
+{
+    public class Repo
+    {
+        
+        
+        
+    }
+}
