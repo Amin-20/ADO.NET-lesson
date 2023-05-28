de1ca63e459f9d539d8c8e9842d37d08b11da94c
Ataki_pw11 <Ataki_pw11@STHQ012D-05>
2023-05-28 18:43:43 +0400

    a

--- WpfApp3/Repository/Repo.cs
+++ WpfApp3/Repository/Repo.cs
@@ -1,16 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp3.Models;
 
 namespace WpfApp3.Repository
 {
     public class Repo
     {
-        
-        
-        
+        ObservableCollection<Author> _authors=new ObservableCollection<Author>();
+        SqlConnection conn;
+        string cs = ConfigurationManager.ConnectionStrings["myConn"].ConnectionString;
+
+        public Repo()
+        {
+            using (conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                SqlDataReader reader = null;
+
+                string query = "SELECT * FROM Authors";
+                using (var command = new SqlCommand(query, conn))
+                {
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            _authors[i].Id = int.Parse(reader[0].ToString());
+                            _authors[i].FirstName = reader[1].ToString();
+                            _authors[i].LastName = reader[2].ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        public ObservableCollection<Author> GetAll()
+        {
+            return _authors;
+        }
+
     }
 }
--- WpfApp3/ViewModels/MainWindowViewModels.cs
+++ WpfApp3/ViewModels/MainWindowViewModels.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using WpfApp3.Commands;
+using WpfApp3.Models;
+using WpfApp3.Repository;
 
 namespace WpfApp3.ViewModels
 {
     public class MainWindowViewModels : BaseViewModel
     {
+
+        public ObservableCollection<Author> authors;
+        public Repo AuthorsRepo { get; set; }
         public RelayCommand InsertCommand { get; set; }
         public RelayCommand DeleteRelayCommand { get; set; }
         public RelayCommand UpdateRelayCommand { get; set; }
@@ -51,11 +57,15 @@ namespace WpfApp3.ViewModels
 
         public MainWindowViewModels()
         {
+            AuthorsRepo = new Repo();
+            authors=new ObservableCollection<Author>(AuthorsRepo.GetAll());
             AuthorsList = new ListBox();
+            AuthorsList.ItemsSource = authors;
+            AuthorsList.DisplayMemberPath = nameof(Author.FirstName);
 
             InsertCommand = new RelayCommand((obj) =>
             {
-                authorsList.Items.Add(FirstName);
+                
 
 
             });
